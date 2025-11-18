ee42aa255671ccdcc29006216f2b89fe855f7daa
nicopinera <nicolaspinera@gmail.com>
2025-11-18 14:08:31 -0300

    GEneracion de CSV

--- TP-05/script/config.py
+++ TP-05/script/config.py
@@ -23,3 +23,6 @@ TOPIC_2_TEMP = "lan/sala2/sensor/temp"
 TOPIC_2_HUM = "lan/sala2/sensor/hum"
 LISTA_TOPIC_PUB = [TOPIC_1_TEMP,TOPIC_1_HUM,TOPIC_2_TEMP,TOPIC_2_HUM]
 TOPIC_ALL = "lan/#"
+
+ARCHIVO = 'datos_sensores.csv'
+MODO = 'a'
--- TP-05/script/sub_5.py
+++ TP-05/script/sub_5.py
@@ -1,10 +1,23 @@
 from pub import Subcriptor
+from pathlib import Path
 import paho.mqtt.client as mqtt
 import time
 import config as con
+import csv
+
+def on_message(client, userdata, msg):
+    texto = msg.payload.decode() # Valor tomado
+    archivo = Path(con.ARCHIVO)
+    escribir_header = not archivo.exists() or archivo.stat().st_size == 0
+    with open(con.ARCHIVO,con.MODO, newline='') as file:
+        escritor = csv.writer(file)
+        if escribir_header:
+            escritor.writerow(["topic", "dato"])
+        escritor.writerow([msg.topic, texto])
 
 gateway = Subcriptor(con.TOPIC_ALL,nombre="Gateway General")
 gateway.conectar(con.BROKER,con.PORT,con.TIME_TO_CONNECT)
+gateway.cliente.on_message = on_message
 gateway.cliente.loop_start()
 
 try:
